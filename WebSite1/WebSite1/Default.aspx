5c966c16241f6cbfb75b0724d2bbae6dad66da5f
yeo weller <kang6171@live.com>
2019-01-03 20:38:42 +0800

    Testing Second Commit

---
+++ WebSite1/WebSite1/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:TextBox ID="TextBox1" runat="server" text="nnnn" Width="287px"></asp:TextBox><br/> <br/>
+        <asp:TextBox ID="TextBox2" runat="server" text="nnnn" Width="287px"></asp:TextBox>
+        
+        <asp:CompareValidator id="Compare1" 
+           ControlToValidate="TextBox2"
+            ErrorMessage="HAHAHAHHAHAAA not same" type="Integer"
+           runat="server" Operator="DataTypeCheck"/>
+    
+    
+    
+        
+    
+    
+    
+    
+        <br />
+        <br />
+        <asp:CheckBox ID="CheckBox1" runat="server" AutoPostBack="true" Checked="True" OnCheckedChanged="CheckBox1_CheckedChanged1" Text="hhhhhhh" />
+    
+    
+    
+        
+    
+    
+    
+    
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Submit" autopostback="true"/>
+        </p>
+    
+    
+    
+        
+    
+    
+    
+    
+    </form>
+
+
+    </body>
+    </html>
---
+++ WebSite1/WebSite1/Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Page.IsPostBack)
+        {
+            CheckBox1.Text = "NICE 2";
+        }
+        else
+        {
+            Response.Write("No post back");
+        }
+    }
+
+    protected void list1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void CheckBoxList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CheckBox1_CheckedChanged1(object sender, EventArgs e)
+    {
+        Response.Write("Happy New year");
+        
+                }
+}
